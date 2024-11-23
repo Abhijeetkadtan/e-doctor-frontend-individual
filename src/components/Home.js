3b12b6e691a9e7f6bf402e4d34b8cd60678c8572
Abhijeetkadtan <abhijeetkadtan47@gmail.com>
2024-11-23 12:49:32 +0530

    updated frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,23 +16,7 @@ function Home() {
                         </div>
                     </div>
                 </header>
-                {/* <section className="features-section">
-            <h2>Why Choose Us?</h2>
-            <div className="features">
-            <div className="feature">
-                <h3>Easy Scheduling</h3>
-                <p>Schedule appointments with ease, whether you're a doctor or a patient.</p>
-            </div>
-            <div className="feature">
-                <h3>Seamless Experience</h3>
-                <p>Enjoy a smooth and user-friendly interface tailored to your needs.</p>
-            </div>
-            <div className="feature">
-                <h3>Secure Platform</h3>
-                <p>Your data is safe with us. Privacy and security are our priorities.</p>
-            </div>
-            </div>
-        </section> */}
+
             </div>
         </body>
     );
